ba30d79b758937079d97a3283f372e5398e5ed2d
VEDANT ADLAK <Vedantadlak2105@gmail.com>
2023-12-26 13:36:12 +0530

    custum hooks made

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+export const GET_RESTAURANTS_URL =
+  "https://swiggyapiwrapper.dineshrout.repl.co/api/restaurants";
 export const CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
 
 export const GITHUB_LINK = "https://github.com/Vedantadlak";
